f2bd7c08c4b66d26dd48eef405d535474e42e661
Jamesstahl98 <Jamesstahl556@gmail.com>
2024-09-05 14:46:46 +0200

    Update Program.cs
    Fixed bug where PrintNumbersBetweenRepeatingNumbers() would continue printing the input with red ForeGroundColor after loop encountered start index number. Added GetSumOfList() function

--- Labb1/Program.cs
+++ Labb1/Program.cs
@@ -6,12 +6,12 @@ namespace Labb1
     {
         static void Main(string[] args)
         {
-            PrintNumbersBetweenRepeatingNumbers("313a3544546746");
+            PrintNumbersBetweenRepeatingNumbers("29535123p48723487597645723645");
         }
 
         static void PrintNumbersBetweenRepeatingNumbers(string input)
         {
-            List<int> numberGroupList = new List<int>();
+            List<long> numberGroupList = new List<long>();
 
             for (int i = 0; i < input.Length; i++)
             {
@@ -19,6 +19,7 @@ namespace Labb1
                 {
                     continue;
                 }
+
                 for (int j = i+1; j < input.Length; j++)
                 {
                     if (!char.IsNumber(input[j]))
@@ -29,32 +30,50 @@ namespace Labb1
                     if (input[j] == input[i])
                     {
                         string currentNumberGroup = string.Empty;
+                        bool isSameNumber = false;
 
                         for (int k = 0; k < input.Length; k++)
                         {
-                            if(k >= i && k <= j)
+                            if(k >= i && k <= j && !isSameNumber)
                             {
                                 currentNumberGroup += input[k];
                                 Console.ForegroundColor = ConsoleColor.Red;
+
+                                if (input[j] == input[k] && k > i)
+                                {
+                                    isSameNumber = true;
+                                }
                             }
                             else
                             {
                                 Console.ForegroundColor = ConsoleColor.Gray;
                             }
+
                             Console.Write(input[k]);
                         }
 
-                        numberGroupList.Add(Int32.Parse(currentNumberGroup));
+                        numberGroupList.Add(Int64.Parse(currentNumberGroup));
                         Console.ForegroundColor = ConsoleColor.Gray;
                         Console.WriteLine();
                         i++;
                     }
                 }
             }
-            for (int i = 0; i < numberGroupList.Count; i++)
-            {
-                Console.WriteLine(numberGroupList[i]);
-            }
+
+            Console.WriteLine($"The sum of the number groups in the string is {GetSumOfList(numberGroupList)}");
         }
+
+        static long GetSumOfList(List<long> list)
+        {
+            long sumOfList = 0;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                sumOfList += list[i];
+            }
+
+            return sumOfList;
+        }
+
     }
 }
