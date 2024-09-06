37867a9d6331217ad74f1710908c3f94e0125240
Jamesstahl98 <Jamesstahl556@gmail.com>
2024-09-06 16:38:12 +0200

    Update Program.cs
    Changed from hard coded input to user input. Changed data type of sum and numberGroups list to ulong. Added exception handling for when group of sumbers is too large for data type

--- Labb1/Program.cs
+++ Labb1/Program.cs
@@ -6,12 +6,14 @@ namespace Labb1
     {
         static void Main(string[] args)
         {
-            PrintNumbersBetweenRepeatingNumbers("29535123p48723487597645723645");
+            Console.WriteLine("Write a string of numbers and letter");
+
+            PrintNumbersBetweenRepeatingNumbers(Console.ReadLine());
         }
 
         static void PrintNumbersBetweenRepeatingNumbers(string input)
         {
-            List<long> numberGroupList = new List<long>();
+            List<ulong> numberGroupList = new List<ulong>();
 
             for (int i = 0; i < input.Length; i++)
             {
@@ -30,18 +32,18 @@ namespace Labb1
                     if (input[j] == input[i])
                     {
                         string currentNumberGroup = string.Empty;
-                        bool isSameNumber = false;
+                        bool isSameAsStartNumber = false;
 
                         for (int k = 0; k < input.Length; k++)
                         {
-                            if(k >= i && k <= j && !isSameNumber)
+                            if(k >= i && k <= j && !isSameAsStartNumber)
                             {
                                 currentNumberGroup += input[k];
                                 Console.ForegroundColor = ConsoleColor.Red;
 
                                 if (input[j] == input[k] && k > i)
                                 {
-                                    isSameNumber = true;
+                                    isSameAsStartNumber = true;
                                 }
                             }
                             else
@@ -52,9 +54,25 @@ namespace Labb1
                             Console.Write(input[k]);
                         }
 
-                        numberGroupList.Add(Int64.Parse(currentNumberGroup));
                         Console.ForegroundColor = ConsoleColor.Gray;
                         Console.WriteLine();
+
+                        ulong numberGroupULong;
+
+                        bool success = UInt64.TryParse(currentNumberGroup, out numberGroupULong);
+                        if(success)
+                        {
+                            numberGroupList.Add(numberGroupULong);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Series of numbers is too big, try a different number");
+                            for (int l = 0; l < numberGroupList.Count; l++)
+                            {
+                                Console.WriteLine(numberGroupList[i]);
+                            }
+                            return;
+                        }
                         i++;
                     }
                 }
@@ -63,9 +81,9 @@ namespace Labb1
             Console.WriteLine($"The sum of the number groups in the string is {GetSumOfList(numberGroupList)}");
         }
 
-        static long GetSumOfList(List<long> list)
+        static ulong GetSumOfList(List<ulong> list)
         {
-            long sumOfList = 0;
+            ulong sumOfList = 0;
 
             for (int i = 0; i < list.Count; i++)
             {
