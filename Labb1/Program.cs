c0350279c00d8ce6d6dacb44d12d5a1443b21d2a
Jamesstahl98 <Jamesstahl556@gmail.com>
2024-09-08 09:23:19 +0200

    Update Program.cs
    Refactoring of PrintNumbersBetweenRepeatingNumbers()

--- Labb1/Program.cs
+++ Labb1/Program.cs
@@ -8,7 +8,7 @@ namespace Labb1
         {
             Console.WriteLine("Write a string of numbers and letter");
 
-            PrintNumbersBetweenRepeatingNumbers(Console.ReadLine());
+            PrintNumbersBetweenRepeatingNumbersNew(Console.ReadLine());
         }
 
         static void PrintNumbersBetweenRepeatingNumbers(string input)
@@ -91,6 +91,58 @@ namespace Labb1
             }
 
             return sumOfList;
+        }
+
+        static void PrintNumbersBetweenRepeatingNumbersNew(string input)
+        {
+            List<ulong> numberGroupList = new List<ulong>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!char.IsNumber(input[i]))
+                {
+                    continue;
+                }
+
+                for (int j = i + 1; j < input.Length; j++)
+                {
+                    if (!char.IsNumber(input[j]))
+                    {
+                        break;
+                    }
+
+                    if (input[i] == input[j])
+                    {
+                        string currentNumberGroup = input.Substring(i, j-i+1);
+
+                        Console.Write(input.Substring(0, i));
+
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(currentNumberGroup);
+
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.Write(input.Substring(j+1));
+                        Console.WriteLine();
+
+                        ulong numberGroupULong;
+
+                        bool success = UInt64.TryParse(currentNumberGroup, out numberGroupULong);
+                        if (success)
+                        {
+                            numberGroupList.Add(numberGroupULong);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Series of numbers is too big, try a different number");
+
+                            return;
+                        }
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine($"The sum of the number groups in the string is {GetSumOfList(numberGroupList)}");
         }
 
     }
